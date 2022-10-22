5e50a38600db9a0af86f75b1b22f610afa319a52
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-22 10:41:09 +0200

    Update Settings.cs

--- DotNetCompress/Model/Settings.cs
+++ DotNetCompress/Model/Settings.cs
@@ -8,7 +8,7 @@ internal class Settings
     public DirectoryInfo? InputDirectory { get; set; } = null;
     public FileInfo[]? OutputFiles { get; set; } = null;
     public DirectoryInfo? OutputDirectory { get; set; } = null;
-    public string[] Pattern { get; set; } = { "*.*" };
+    public string[] Pattern { get; set; } = {"*.*"};
     public string Format { get; set; } = "br";
     public CompressionLevel Level { get; set; } = CompressionLevel.SmallestSize;
     public int Threads { get; set; } = 1;
